3cf068aeec57e5429c48b2a715ab9df1263a8763
Michael Musgrove <mmusgrov@redhat.com>
2017-11-04 16:51:04 +0000

    step3 Create flight service and deploy in main. Update vertx.launcher in the pom

--- mucon/backup/src/main/java/io/narayana/mucon/MainVerticle.java
+++ mucon/backup/src/main/java/io/narayana/mucon/MainVerticle.java
@@ -1,16 +1,26 @@
 package io.narayana.mucon;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
+
+    private static FlightService flightService;
+
+    public static void main(String[] args) {
+        flightService = new FlightServiceImpl();
+
+        Vertx.vertx().deployVerticle(MainVerticle.class.getName());
+    }
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
 
         router.post("/api").handler(request -> {
-            request.response().end(Thread.currentThread().getName());
+            flightService.createBooking("BA123");
+            request.response().end(Thread.currentThread().getName() + ": Booking Count: " + flightService.getNumberOfBookings());
         });
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
