ee015852c8df256deffbb536c910cdb466650dfa
Michael Musgrove <mmusgrov@redhat.com>
2017-11-04 16:44:38 +0000

    step2 Add route for posting to /api

--- mucon/backup/src/main/java/io/narayana/mucon/MainVerticle.java
+++ mucon/backup/src/main/java/io/narayana/mucon/MainVerticle.java
@@ -1,12 +1,19 @@
 package io.narayana.mucon;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
+        Router router = Router.router(vertx);
 
+        router.post("/api").handler(request -> {
+            request.response().end(Thread.currentThread().getName());
+        });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
     }
 
 }
